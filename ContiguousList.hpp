496156434ad2cac9d57cfa7635b0f654582c7eb8
Chuan Qin <qc2105@qq.com>
2019-04-23 00:11:03 +0800

    ContiguousList with template

--- ContiguousList.hpp
+++ ContiguousList.hpp
@@ -2,6 +2,9 @@
 #define CONTIGUOUS_LIST_HPP
 
 typedef int Error_code;
+const int success = 0;
+const int overflow = 1;
+const int range_error = -2;
 const int max_list = 1000;
 
 template <class List_entry>
@@ -42,5 +45,77 @@ class List
     int count;
     List_entry entry[max_list];
 };
+
+template <class List_entry>
+List<List_entry>::List()
+{
+    count = 0;
+}
+
+template <class List_entry>
+void List<List_entry>::clear()
+{
+    count = 0;
+}
+
+template <class List_entry>
+int List<List_entry>::size() const
+{
+    return count;
+}
+
+template <class List_entry>
+bool List<List_entry>::empty() const
+{
+    return count == 0;
+}
+
+template <class List_entry>
+bool List<List_entry>::full() const
+{
+    return count == max_list;
+}
+
+template <class List_entry>
+Error_code List<List_entry>::retrieve(int postion, List_entry &x) const
+{
+    if (postion >= 0 && postion < count)
+    {
+        x = entry[postion];
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+template <class List_entry>
+Error_code List<List_entry>::insert(int position, const List_entry &x)
+{
+    if (full())
+    {
+        return overflow;
+    }
+    if (position < 0 || position > count)
+    {
+        return range_error;
+    }
+    for (int i = count - 1; i >= position; i--)
+    {
+        entry[i + 1] = entry[i];
+    }
+    entry[position] = x;
+    count++;
+    return success;
+}
+
+template <class List_entry>
+void List<List_entry>::traverse(void(*visit)(List_entry &))
+{
+    for (int i = 0; i < count; i++)
+    {
+        (*visit)(entry[i]);
+    }
+}
 
 #endif
