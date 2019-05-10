81156a46c94649f88e7d97570dc8fbb48d5939eb
Chuan Qin <qc2105@qq.com>
2019-05-10 21:28:05 +0800

    add more tests

--- testContiguousList.cpp
+++ testContiguousList.cpp
@@ -14,11 +14,28 @@ TEST(TestContiguousList, test_size)
     ASSERT_EQ(myList.size(), 0);
 }
 
+TEST(TestContiguousList, test_function_full)
+{
+    List<int> myList = List<int>();
+    ASSERT_FALSE(myList.full());
+}
+
+TEST(TestContiguousList, test_insert_retrieve)
+{
+    List<int> myList = List<int>();
+    ASSERT_EQ(myList.insert(0, 5), success);
+    ASSERT_EQ(myList.size(), 1);
+    int x, y;
+    myList.retrieve(0, x);
+    myList.insert(1, 6);
+    ASSERT_EQ(x, 5);
+    myList.retrieve(1, y);
+    ASSERT_EQ(y, 6);
+}
+
 int main(int argc, char *argv[])
 {
     InitGoogleTest();
 
-    RUN_ALL_TESTS();
-
-    return 0;
+    return RUN_ALL_TESTS();
 }
